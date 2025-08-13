55d9496cdccb84e12e47168b66303d298aa98611
bluess57 <ijhope@internode.on.net>
2025-08-13 15:15:18 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Set path to a python virtual environment
-pythonvenv="/home/ian/gt7dashboard-1/python/bin"
+pythonvenv="/home/ian/gt7dashboard-1/python/bin" # Adjust this path as needed to a virtual python environment
 
 $pythonvenv/pip3 install -r requirements.txt
 
 $pythonvenv/python3 helper/download_cars_csv.py
 
+export GT7_LOG_LEVEL="DEBUG" # Change to "INFO" or "ERROR" as needed
+export GT7_PLAYSTATION_IP="192.168.1.103"  # Replace with your actual IP address
 $pythonvenv/python3 -m bokeh serve --show .
